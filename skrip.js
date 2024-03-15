cdcd94861a0179df9f2489172d20391d4827dfb9
ArvieIrfan <downb805@gmail.com>
2024-03-15 17:20:50 +0700

    penambahan pengumpulan tugas javascript latihan ke-12

--- skrip.js
+++ skrip.js
@@ -1,7 +1,15 @@
 function dataPerson(){
     let forms = document.getElementById('frm')
     let nama = forms.nama.value
-    
+    let kerjaan = forms.kerjaan.value
+    let hobi = forms.hoby.value
+
+    let ket = "Halo teman teman <br> nama saya adalah " + nama + " pekerjaan saya sekarang adalah sebagai seorang " + kerjaan
+               + "<br> dan saya memiliki berbagai hobi yang di mana hobi saya adalah " + hobi
+    let no_ket = "Ada data yang tidak diisi dimohon untuk mengisi semua data yang ada"
+    let hasil = (nama && kerjaan && hobi != '') ? ket : no_ket
+
+    document.getElementById('hasil').innerHTML = hasil
 
     // tugasnya adalah lengkapi kode berikut dan tampilkan hasilnya pada HTML
     // 1. isian nama
